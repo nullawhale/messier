5e62f132b922a5f338f5e2226ab1bca16effce92
nliapushkin <nliapushkin@bivgroup.com>
2020-08-31 21:41:00 +0300

    Add simple gui (gtk-rs)

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,249 @@
+extern crate gio;
+extern crate gtk;
+extern crate glib;
+
 use std::{
-    fs::{self, DirEntry},
-    path::Path,
-    time::SystemTime
+    fs::{self},
+    path::{Path, PathBuf},
+    rc::Rc,
+    time::SystemTime,
+    sync::atomic::{AtomicBool, Ordering}
 };
+
+use gio::prelude::*;
+use gtk::{Application, ApplicationWindow, Button, Entry};
+use gtk::prelude::*;
 
 // File struct
 #[derive(Debug)]
 struct File {
     name: String,
-    size: u64,
+    size: String,
+    o_type: String,
     modified: String
 }
 
 impl File {
-    pub fn new(name: String, size: u64, modified: String) -> Self {
-        File { name, size, modified }
+    pub fn new(name: String, size: String, o_type: String, modified: String) -> Self {
+        File { name, size, o_type, modified }
     }
 }
 
 // Dir struct
-#[derive(Debug)]
+/*#[derive(Debug)]
 struct Dir {
     name: String,
+    size: String,
+    o_type: String,
+    modified: String
 }
 
 impl Dir {
-    pub fn new(name: String) -> Self {
-        Dir { name }
+    pub fn new(name: String, modified: String) -> Self {
+        Dir { name, size: "0".to_string(), o_type: "File".to_string(), modified}
     }
+}*/
+
+#[repr(i32)]
+enum Columns {
+    Name,
+    Size,
+    Type,
+    Modified,
 }
+
+static SHOW_HIDDEN: AtomicBool = AtomicBool::new(false);
 
 fn main() {
-    let (mut files, mut dirs) = get_files_and_dirs(Path::new("."));
-
-    dirs.sort_by(|a, b| a.name.cmp(&b.name));
-    for dir in dirs {
-        println!("{}", dir.name);
-    }
+    let path = PathBuf::from(".");
+    /*let mut files = get_files_and_dirs(path.as_path());
 
     files.sort_by(|a, b| a.name.cmp(&b.name));
-    for file in files {
-        println!("{} ({}) ({})", file.name, file.size, file.modified);
-    }
+    for file in &files {
+        println!("{} ({}) ({}) ({})", file.name, file.size, file.o_type, file.modified);
+    }*/
+
+    let abs_pathbuf = fs::canonicalize(&path).unwrap();
+
+    let application = Application::new(
+        Some("com.github.gtk-rs.examples.basic"),
+        Default::default(),
+    ).expect("failed to initialize GTK application");
+
+    application.connect_activate(move |app| {
+        let window = ApplicationWindow::new(app);
+        window.set_title("Messier files");
+        window.set_position(gtk::WindowPosition::Center);
+        window.set_default_size(800, 600);
+
+        let vbox = gtk::Box::new(gtk::Orientation::Vertical, 8);
+        window.add(&vbox);
+
+        let label = gtk::Label::new(abs_pathbuf.to_str());
+        vbox.add(&label);
+
+        let sw = gtk::ScrolledWindow::new(None::<&gtk::Adjustment>, None:: <&gtk::Adjustment>);
+        sw.set_shadow_type(gtk::ShadowType::EtchedIn);
+        sw.set_policy(gtk::PolicyType::Never, gtk::PolicyType::Automatic);
+        vbox.add(&sw);
+
+        let model = Rc::new(create_model(get_files_and_dirs(path.as_path(), SHOW_HIDDEN.load(Ordering::SeqCst)).as_slice()));
+        let tree_view = gtk::TreeView::with_model(&*model);
+        tree_view.set_vexpand(true);
+
+        sw.add(&tree_view);
+        add_columns(&tree_view);
+
+        let button = Button::with_label("I'm useless!");
+        button.connect_clicked(move |_| {
+            // gtk::TreeView::
+            // unsafe { tree_view.set_data }
+            let show_hidden: bool = SHOW_HIDDEN.load(Ordering::SeqCst);
+            SHOW_HIDDEN.fetch_nand(show_hidden, Ordering::SeqCst);
+            println!("{:?}", SHOW_HIDDEN.load(Ordering::SeqCst));
+
+        });
+        vbox.add(&button);
+
+        window.show_all();
+
+        let entry = Entry::new();
+        entry.connect_key_press_event(|_, _| {
+            println!("key pressed");
+            Inhibit(false)
+        });
+    });
+
+    application.run(&[]);
 }
 
-fn get_files_and_dirs(dir: &Path) -> (Vec<File>, Vec<Dir>) {
+fn get_files_and_dirs(dir: &Path, show_hidden: bool) -> Vec<File> {
     let mut files: Vec<File> = Vec::new();
-    let mut dirs: Vec<Dir> = Vec::new();
 
     let paths = fs::read_dir(dir).unwrap();
 
     for path in paths {
-        if let Ok(path) = path {
-            if let Ok(metadata) = path.metadata() {
-                if metadata.is_dir() {
-                    dirs.push(Dir::new(path.file_name().into_string().unwrap()));
-                } else if metadata.is_file() {
-                    let mut time: SystemTime = metadata.modified().unwrap();
-                    files.push(File::new(
-                        path.file_name().into_string().unwrap(),
-                        metadata.len(),
-                        systime_to_secs(time)
-                    ));
-                }
-            }
+        let path = path.unwrap();
+        let metadata = path.metadata().unwrap();
+        let time: SystemTime = metadata.modified().unwrap();
+        let name = path.file_name().into_string().unwrap();
+
+        if metadata.is_dir() {
+            files.push(File::new(
+                name,
+                "0".to_string(),
+                "Folder".to_string(),
+                format_systime(time)
+            ));
+        } else if metadata.is_file() {
+            files.push(File::new(
+                name,
+                format_filesize(metadata.len()),
+                "File".to_string(),
+                format_systime(time)
+            ));
         }
     }
 
-    (files, dirs)
+    if show_hidden == true {
+        files
+    } else {
+        files.into_iter().filter(|f| !f.name.starts_with('.')).collect()
+    }
 }
 
-fn systime_to_secs(time: SystemTime) -> String {
+fn format_systime(time: SystemTime) -> String {
     let datetime: chrono::DateTime<chrono::offset::Utc> = time.into();
-    (datetime.format("%d %m %Y %H:%M").to_string())
+    datetime.format("%d-%m-%Y %H:%M").to_string()
+}
+
+fn format_filesize(bytes: u64) -> String {
+    const UNITS: [&str; 8] = ["Bytes", "kB", "MB", "GB", "TB", "PB", "EB", "ZB"];
+    const THRESH: f64 = 1024.0;
+    let mut bytes: f64 = bytes as f64;
+
+    let mut index = 0;
+    while bytes > THRESH {
+        bytes /= THRESH;
+        index += 1;
+    }
+
+    if bytes.fract() == 0.0 {
+        format!("{:.0} {}", bytes, UNITS[index])
+    } else {
+        format!("{:.1} {}", bytes, UNITS[index])
+    }
+}
+
+fn create_model(files: &[File]) -> gtk::ListStore {
+
+    let col_types: [glib::Type; 4] = [
+        glib::Type::String,
+        glib::Type::String,
+        glib::Type::String,
+        glib::Type::String,
+    ];
+
+    let store = gtk::ListStore::new(&col_types);
+
+    let col_indices: [u32; 4] = [0, 1, 2, 3];
+
+    for (_, d) in files.iter().enumerate() {
+        let values: [&dyn ToValue; 4] = [
+            &d.name,
+            &d.size,
+            &d.o_type,
+            &d.modified,
+        ];
+        store.set(&store.append(), &col_indices, &values);
+    }
+
+    store
+}
+
+fn add_columns(tree_view: &gtk::TreeView) {
+    // Column for Name
+    {
+        let renderer = gtk::CellRendererText::new();
+        let column = gtk::TreeViewColumn::new();
+        column.pack_start(&renderer, true);
+        column.set_title("Name");
+        column.add_attribute(&renderer, "text", Columns::Name as i32);
+        column.set_sort_column_id(Columns::Name as i32);
+        column.set_expand(true);
+        tree_view.append_column(&column);
+    }
+
+    // Column for Size
+    {
+        let renderer = gtk::CellRendererText::new();
+        let column = gtk::TreeViewColumn::new();
+        column.pack_start(&renderer, true);
+        column.set_title("Size");
+        column.add_attribute(&renderer, "text", Columns::Size as i32);
+        column.set_sort_column_id(Columns::Size as i32);
+        tree_view.append_column(&column);
+    }
+
+    // Column for Type
+    {
+        let renderer = gtk::CellRendererText::new();
+        let column = gtk::TreeViewColumn::new();
+        column.pack_start(&renderer, true);
+        column.set_title("Type");
+        column.add_attribute(&renderer, "text", Columns::Type as i32);
+        column.set_sort_column_id(Columns::Type as i32);
+        tree_view.append_column(&column);
+    }
+
+    // Column for Modified
+    {
+        let renderer = gtk::CellRendererText::new();
+        let column = gtk::TreeViewColumn::new();
+        column.pack_start(&renderer, true);
+        column.set_title("Modified");
+        column.add_attribute(&renderer, "text", Columns::Modified as i32);
+        column.set_sort_column_id(Columns::Modified as i32);
+        tree_view.append_column(&column);
+    }
 }
